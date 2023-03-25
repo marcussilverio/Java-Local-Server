e1ba0de4d0201cdcb2ad41fb5879809a0ba4eb22
Marcus Silverio - W11 G15 <marcus_vs@live.com>
2023-03-25 19:21:06 -0300

    Adicionado tratamento de erro para quando o arquivo nao eh encontrado e Log de requisicao

--- src/main/java/com/github/marcussilverio/Main.java
+++ src/main/java/com/github/marcussilverio/Main.java
@@ -3,21 +3,23 @@ package com.github.marcussilverio;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.jboss.logging.Logger;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.http.HttpResponse;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     int port = 8000;
-    String filePath = "C:\\Downloads\\imdb-Top250Movies.JSON\\";;
+    String filePath = "D:\\Downloads\\imdb-Top250Movies.JSON\\";;
 
     HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
     server.createContext("/", new FileHandler(filePath));
-    server.setExecutor(null); // creates a default executor
+    server.setExecutor(null);
     server.start();
   }
 
@@ -30,14 +32,23 @@ public class Main {
 
     public void handle(HttpExchange t) throws IOException {
       byte[] bytes = readBytesFromFile(filePath);
-      t.sendResponseHeaders(200, bytes.length);
+      if(bytes == null){
+        bytes  = "ERROR - File not found".getBytes();
+        t.sendResponseHeaders(404, bytes.length);
+      }else {
+        t.sendResponseHeaders(200, bytes.length);
+      }
       OutputStream os = t.getResponseBody();
       os.write(bytes);
+      System.out.println("[ERROR] => Request " + t.getRemoteAddress() + " responseCode: "+ t.getResponseCode());
       os.close();
     }
 
     private static byte[] readBytesFromFile(String filePath) throws IOException {
       File file = new File(filePath);
+      if (!file.exists()){
+        return null;
+      }
       byte[] bytes = new byte[(int) file.length()];
       FileInputStream fis = new FileInputStream(file);
       fis.read(bytes);
